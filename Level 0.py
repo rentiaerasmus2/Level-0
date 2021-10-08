f0af95020f142b9dcc8dcf01d24ce7156ae6fe93
rentiaerasmus2 <91526974+rentiaerasmus2@users.noreply.github.com>
2021-10-08 13:18:10 +0200

    Add files via upload

---
+++ Level 0.py
@@ -0,0 +1,109 @@
+Task 0.1
+
+x = 0
+print(x)
+
+y = 1
+print(y)
+
+x = x + 3
+print(x)
+
+y = y + x
+print(y)
+
+
+Task 0.2
+
+x = 1 + 1 * 2
+y = (1 + 1) * 2
+z = 1 + (1 * 2)
+a = 1 + 1 * 2 / 2
+b = (1 + 1 * 2) / 2
+
+print(x, y, z, a, b)
+
+Task 0.3
+
+name = input("please enter your name")
+print("Hello" + " " + name + "!")
+ """
+
+Task 0.4
+
+""" num = int(input("Enter a number: "))
+if (num % 2) == 0:
+print("{0} is Even number".format(num))
+else:
+print("{0} is Odd number".format(num))
+"""
+
+
+Task 0.5
+
+""" h = input("pls enter hight")
+b = input("pls enter base")
+
+area = float(0.5(h + b))
+print(area)
+"""
+
+
+Task 0.6
+
+""" x = int(input('Enter First Number: '))
+y = int(input('Enter Second Number: '))
+z = int(input('Enter Third Number '))
+print("The highest value entered is:",max(x,y,z))
+
+Variable = int(input("How many numbers do you want to enter?: "))
+
+Numbers = []
+
+for i in range(Variable):
+    new = input('Enter number {}: '.format(i+1))
+
+print("Maximum Value: ",max(range))
+
+Task 0.7
+
+""" "c = float(input("temp"))
+
+f = (c * 1.8) + 32
+
+print(f) """
+
+"""f = float(input("fahrenheit"))
+
+c = (f - 32) * 5/9
+
+print(c)"""
+
+""""Task 0.8
+
+import datetime
+sec = int(input("insert second: "))
+res = datetime.timedelta(seconds=sec)
+print(res)
+
+Task 0.9
+"""
+"""voul = input("insert word: ")
+print(voul)
+
+for let in voul:
+if let == 'a' or let == 'e' or let == 'i' or let == 'o' or let == 'u':
+print(let) """
+
+Task 0.10
+
+#usr1 = input("input: ")
+#usr2 = input("input: ")
+
+#s1 = set(usr1)
+#s2 = set(usr2)
+#lst = list(s1 & s2)
+#print("c letter: {}" .format(lst))
+
+
+
