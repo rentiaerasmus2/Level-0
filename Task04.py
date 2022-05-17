d9e2677c20d3466f832b1e245ab12426a811d516
rentiaerasmus2 <91526974+rentiaerasmus2@users.noreply.github.com>
2022-05-17 10:01:25 +0200

    Update and rename Task04.py to task0_04.py

--- Task04.py
+++
@@ -1,7 +0,0 @@
-a = 4
-b = 3
-if a > b:
-        print("even")
-else:
-        print("odd") 
-
---
+++ task0_04.py
@@ -0,0 +1,17 @@
+def even_or_odd(number):
+        if number%2 == 0:
+                numbertype ="even"
+        else:
+                numbertype ="odd"
+        
+        print(numbertype)
+
+
+
+number = int (3)
+numbertype = even_or_odd(number)
+
+
+
+even_or_odd
+
