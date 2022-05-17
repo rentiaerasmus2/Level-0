d0c5ed28a3727979026adff14afe6295ec9ef27a
rentiaerasmus2 <91526974+rentiaerasmus2@users.noreply.github.com>
2022-05-17 09:49:19 +0200

    Update and rename Task07.py to task0_07.py

--- Task07.py
+++
@@ -1,5 +0,0 @@
-celsius = 24
-
-fahrenheit = (celsius * 1.8) + 32
-
-print("%.2f Celsius = %.2f Fahrenheit" %( celsius, fahrenheit ))
---
+++ task0_07.py
@@ -0,0 +1,7 @@
+def convert_celsius_to_fahrenheit(celsius):
+
+    fahrenheit = (celsius * 1.8) + 32
+    return fahrenheit
+
+
+print(convert_celsius_to_fahrenheit(24))
